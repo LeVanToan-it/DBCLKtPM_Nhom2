6f3c192cb7db69ab076a0280c75b9ddf417e2435
LeVanToan <lvtoan.it2000@gmail.com>
2022-03-26 00:14:34 +0700

    bt nhom 2

---
+++ DHKTPM15A_TOAN_SON_BTNHOM/DHKTPM15A_TOAN_SON_BTNHOM/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DHKTPM15A_TOAN_SON_BTNHOM")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DHKTPM15A_TOAN_SON_BTNHOM")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c8a8589d-3649-40d0-90ec-aaadd8a2d201")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DHKTPM15A_TOAN_SON_BTNHOM/DHKTPM15A_TOAN_SON_BTNHOM/utLab1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DHKTPM15A_TOAN_SON_BTNHOM
+{
+    [TestClass]
+    public class utLab1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
